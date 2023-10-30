c48eba0ce5547bb14fd0d4a4f097dcecdef1fbd8
codeviith <codeviith@Cinya.>
2023-10-30 11:27:10 -0400

    front and backend server working

--- server/models.py
+++ server/models.py
@@ -17,7 +17,7 @@ class Message(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    
+
 
 
 
